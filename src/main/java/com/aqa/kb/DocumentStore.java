03554785c0930b031728e5e1f3b90d1fb0494da3
Scott Miles <spm2229@utexas.edu>
2016-09-20 00:01:36 -0500

    Massaged corpus documents even more. Got file input working ocrrectly

--- src/main/java/com/aqa/kb/DocumentStore.java
+++ src/main/java/com/aqa/kb/DocumentStore.java
@@ -1,11 +1,28 @@
 package com.aqa.kb;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class DocumentStore {
 
     /*
      * Flags to print extra information and show statistics
      */
     private boolean explicit, stats;
+
+    // Unfortunately had to hard code the file names
+    private String[] filenames = {
+        "/corpus/american-crocodile.txt", "/corpus/cuban-crocodile.txt",
+        "/corpus/freshwater-crocodile.txt", "/corpus/morelets-crocodile.txt",
+        "/corpus/mugger-crocodile.txt", "/corpus/new-guinea-crocodile.txt",
+        "/corpus/nile-crocodile.txt", "/corpus/orinoco-crocodile.txt",
+        "/corpus/phillipine-crocodile.txt", "/corpus/saltwater-crocodile.txt",
+        "/corpus/siamese-crocodile.txt", "/corpus/west-african-crocodile.txt"
+    };
     
     public DocumentStore(boolean explicit, boolean stats) {
         this.explicit = explicit;
@@ -14,8 +31,28 @@ public class DocumentStore {
 
     public void loadDocuments() {
         System.out.println("We need code to load the documents");
-        // Need to add code to read in the files here and load them 
-        //  into a data structure that can handle all their information.
+
+        try {        
+
+            ClassLoader classLoader = this.getClass().getClassLoader();
+
+            for (String filename : filenames){
+
+
+                InputStream in = getClass().getResourceAsStream(filename);
+                Scanner scan = new Scanner(in);
+                
+                while(scan.hasNextLine()) {
+                    System.out.println(scan.nextLine());
+                }
+
+            }
+        } catch(Exception fofe) {
+            fofe.printStackTrace();
+        }
+        System.out.println();
+
+
     }
 
 }
---
+++ src/main/java/com/aqa/kb/TripleStore.java
@@ -0,0 +1,12 @@
+package com.aqa.kb;
+
+public class TripleStore {
+
+    private boolean explicit, stats;
+
+    public TripleStore(boolean explicit, boolean stats) {
+        this.explicit = explicit;
+        this.stats = stats;
+    }
+
+}
